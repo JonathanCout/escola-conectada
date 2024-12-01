90b0ccf9906ef89805131ca75a577f1a4fe331f6
Jonathan Coutinho <85229845+JonathanCout@users.noreply.github.com>
2024-12-01 16:30:51 -0300

    fix - fixed Aluno/Parente query... again

--- backend/src/main/java/com/unicarioca/portal/repository/AlunoRepository.java
+++ backend/src/main/java/com/unicarioca/portal/repository/AlunoRepository.java
@@ -12,6 +12,6 @@ public interface AlunoRepository extends JpaRepository<Aluno, Long> {
     Aluno findByEmail(String email);
 
     @Modifying
-    @Query(value = "DELETE FROM parente_aluno where aluno_id = ?1")
+    @Query(value = "DELETE FROM parente_aluno where aluno_id = ?1", nativeQuery = true)
     void removeParenteJunction(Long alunoId);
 }
