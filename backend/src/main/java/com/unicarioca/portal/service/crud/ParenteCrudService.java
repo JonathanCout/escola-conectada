23a60a813b1ccdd7ee82b670464914b044a98f51
Jonathan Coutinho <85229845+JonathanCout@users.noreply.github.com>
2024-12-01 13:55:39 -0300

    feat - Added log for debbuging

--- backend/src/main/java/com/unicarioca/portal/service/crud/ParenteCrudService.java
+++ backend/src/main/java/com/unicarioca/portal/service/crud/ParenteCrudService.java
@@ -5,12 +5,15 @@ import com.unicarioca.portal.entity.Parente;
 import com.unicarioca.portal.repository.ParenteRepository;
 import com.unicarioca.portal.service.mapper.ParenteMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParenteCrudService {
 
+    private final Logger log = LoggerFactory.getLogger(ParenteCrudService.class);
     @Autowired
     private ParenteRepository parenteRepository;
 
@@ -19,6 +22,7 @@ public class ParenteCrudService {
     }
 
     public Parente saveParente(ParenteRequest parenteRequest){
+        log.info("Salvando parente={}", parenteRequest);
         Parente parente = getParenteByCpf(parenteRequest.getCpf());
         if (parente != null){
             return parente;
