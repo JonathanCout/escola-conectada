9bc7b1cea91eee0c208caacb92404d5710fcf686
Jonathan Coutinho <85229845+JonathanCout@users.noreply.github.com>
2024-12-01 15:40:59 -0300

    fix - fixed Aluno/Parente query

--- backend/src/main/java/com/unicarioca/portal/repository/AlunoRepository.java
+++ backend/src/main/java/com/unicarioca/portal/repository/AlunoRepository.java
@@ -2,6 +2,7 @@ package com.unicarioca.portal.repository;
 
 import com.unicarioca.portal.entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
@@ -10,6 +11,7 @@ public interface AlunoRepository extends JpaRepository<Aluno, Long> {
     Aluno findByCpf(String cpf);
     Aluno findByEmail(String email);
 
-    @Query(value = "DELETE FROM parente_aluno where aluno_id = ?1", nativeQuery = true)
+    @Modifying
+    @Query(value = "DELETE FROM parente_aluno where aluno_id = ?1")
     void removeParenteJunction(Long alunoId);
 }
