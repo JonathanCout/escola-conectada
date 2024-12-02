2b4e89a8f25a4584e0bd3f40e5ad3a0effc805f7
Jonathan Coutinho <85229845+JonathanCout@users.noreply.github.com>
2024-12-02 20:41:02 -0300

    feat - added validation for same professor

--- backend/src/main/java/com/unicarioca/portal/service/ProfessorService.java
+++ backend/src/main/java/com/unicarioca/portal/service/ProfessorService.java
@@ -2,8 +2,11 @@ package com.unicarioca.portal.service;
 
 import com.unicarioca.portal.controller.dto.ProfessorRequest;
 import com.unicarioca.portal.controller.dto.ProfessorResponse;
+import com.unicarioca.portal.entity.Professor;
 import com.unicarioca.portal.service.crud.ProfessorCrudService;
 import com.unicarioca.portal.service.mapper.ProfessorMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,8 @@ import java.util.List;
 
 @Service
 public class ProfessorService {
+
+    private final Logger log = LoggerFactory.getLogger(ProfessorService.class);
 
     @Autowired
     private ProfessorCrudService professorCrudService;
@@ -23,7 +28,13 @@ public class ProfessorService {
         return ProfessorMapper.toDto(professorCrudService.getProfessor(cpf, matricula, email));
     }
 
-    public ProfessorResponse saveProfessor(ProfessorRequest professorRequest) {
+    public ProfessorResponse saveProfessor(ProfessorRequest professorRequest) throws Exception {
+        Professor professor = professorCrudService.getProfessor(professorRequest.getCpf(), professorRequest.getMatricula(), professorRequest.getEmail());
+        if (professor != null){
+            log.warn("Já existe um professir cadastrado com as informações fornecidas");
+            throw new Exception("Já existe um professir cadastrado com as informações fornecidas");
+        }
+
         return ProfessorMapper.toDto(professorCrudService.saveProfessor(ProfessorMapper.toEntity(professorRequest)));
     }
 
